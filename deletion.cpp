814861c1b56e4661d2482b2ebeffef1e3dc03c58
Tanishka Saxena <tanishkasaxena13@gmail.com>
2024-04-26 10:37:58 +0530

    Deletion in linked list

---
+++ deletion.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insert(node *&head, int val)
+{
+    node *n = new node(val);
+
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void deletehead(node *&head)
+{
+    node *todelete = head;
+    head = head->next;
+
+    delete todelete;
+}
+
+void deletion(node *&head, int val)
+{
+
+    node *temp = head;
+    while (temp->next->data != val)
+    {
+        temp = temp->next;
+    }
+    node *todelete = temp->next;
+    temp->next = temp->next->next;
+
+    delete todelete;
+}
+
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main()
+{
+    node *head = NULL;
+    insert(head, 1);
+    insert(head, 2);
+    insert(head, 3);
+    display(head);
+    // deletion(head, 3);
+    deletehead(head);
+    cout << "AFTER DELETION: " << endl;
+    display(head);
+}
